e1705984de0897528c2c86913bb530837b3e05e8
Peter Amerkhanian <peteamer@gmail.com>
2019-01-17 09:22:18 -0500

    Moved 'blank' into te function args, ask user for the last row occupied by grades

--- grade_manager.py
+++ grade_manager.py
@@ -2,17 +2,17 @@ import pyautogui
 import xlwings as xw
 
 
-def copy_paste(data, reverse=False):
+def copy_paste(data, reverse=False, blank=True):
     """
     data - a list
     reverse - boolean
     """
     for score in data[:-1]:
-        if blank == "N":
+        if not blank:
             pyautogui.PAUSE = 0.1
             for _ in range(5):
                 pyautogui.press('backspace')
-        pyautogui.PAUSE = 0.3
+        pyautogui.PAUSE = 0.2
         pyautogui.typewrite(score)
         if reverse:
             pyautogui.press('up')
@@ -26,12 +26,13 @@ if __name__ == '__main__':
     # init pyautogui fail safe
     pyautogui.FAILSAFE = True
     # start program
-    print("This program will be extracting grades from an Excel file.")
+    print("...\n...\nThis program will be extracting grades from an Excel file.")
     print("Make sure that you have your desired Excel file open, and the correct sheet selected,")
     input("then press 'Enter'")
+    last_row = input("...\n...\nWhat is the last row occupied by students? ")
     while True:
-        blank = input("...\n...\nIs the spreadsheet you will be copying the information into currently blank? (Y/N) "
-                      "Note: If you are copying into a spreadsheet app that supports overwriting cells, just select Y")
+        blank = input("...\n...\nIs the spreadsheet you will be copying the information into currently blank? (Y/N)"
+                      "\n(If you are copying into a spreadsheet app that supports overwriting cells, just select Y) ")
         if blank.lower() == "y" or blank.lower() == "n":
             break
         else:
@@ -42,19 +43,26 @@ if __name__ == '__main__':
     else:
         columns = ['C', 'H', 'M', 'R', 'W']
     # begin entering data
-    input("\n\nMove your mouse to the first cell then press 'Enter'"
+    input("...\n...\nMove your mouse to the first cell then press 'Enter'"
           " \n ***Leave the computer alone after you press Enter!*** "
-          " \n ***Move your mouse to the upper left hand corner of the screen to stop the process!***")
+          " \n ***Move your mouse to the upper left hand\n corner of the screen to stop the process!***"
+          "...\n...\n")
     pyautogui.click(pyautogui.position())
     for ind, col in enumerate(columns):
         grade_dict = {}
         if ind % 2 == 0:
-            assignment = [str(score) for score in xw.Range(f'{col}11:{col}42').value]
+            assignment = [str(score) for score in xw.Range(f'{col}11:{col}{last_row}').value]
             grade_dict['column'] = col
             grade_dict['scores'] = assignment
-            copy_paste(grade_dict['scores'])
+            if blank == 'y':
+                copy_paste(grade_dict['scores'])
+            else:
+                copy_paste(grade_dict['scores'], blank=False)
         else:
-            assignment = [str(score) for score in list(xw.Range(f'{col}11:{col}42').value)[::-1]]
+            assignment = [str(score) for score in list(xw.Range(f'{col}11:{col}{last_row}').value)[::-1]]
             grade_dict['column'] = col
             grade_dict['scores'] = assignment
-            copy_paste(grade_dict['scores'], reverse=True)
+            if blank == 'y':
+                copy_paste(grade_dict['scores'], reverse=True)
+            else:
+                copy_paste(grade_dict['scores'], reverse=True, blank=False)
