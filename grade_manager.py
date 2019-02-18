ca46b6bfdc7ff17a70316d4844c152e5b0f92e97
Peter Amerkhanian <peteamer@gmail.com>
2019-02-18 16:38:14 -0500

    pre-major changes

--- grade_manager.py
+++ grade_manager.py
@@ -9,11 +9,11 @@ def copy_paste(data, reverse=False, blank=True):
     data - a list
     reverse - boolean
     """
-    count = 0
+    row_count = 0
     for score in data[:-1]:
         if not blank:
             pyautogui.hotkey('ctrl', 'c')
-            time.sleep(.01)
+            time.sleep(.02)
             text = str(pyperclip.paste())
             while True:
                 if "0.01" in text:
@@ -21,9 +21,9 @@ def copy_paste(data, reverse=False, blank=True):
                         pyautogui.press('up')
                     else:
                         pyautogui.press('down')
-                        count += 1
+                        row_count += 1
+                    time.sleep(.02)
                     pyautogui.hotkey('ctrl', 'c')
-                    time.sleep(.01)
                     text = str(pyperclip.paste())
                 else:
                     break
@@ -32,14 +32,14 @@ def copy_paste(data, reverse=False, blank=True):
                 pyautogui.press('backspace')
         pyautogui.PAUSE = 0.3
         pyautogui.typewrite(score)
-        time.sleep(.01)
+        time.sleep(.02)
         if reverse:
             pyautogui.press('up')
-            time.sleep(.01)
+            time.sleep(.02)
         else:
             pyautogui.press('down')
-            count += 1
-            time.sleep(.01)
+            row_count += 1
+            time.sleep(.02)
     pyautogui.PAUSE = 0.1
     for _ in range(len(text)):
         pyautogui.press('backspace')
@@ -47,21 +47,51 @@ def copy_paste(data, reverse=False, blank=True):
     pyautogui.typewrite(data[-1])
     time.sleep(.02)
     pyautogui.press('right')
-    return count
+    return row_count
 
 
 def cleanup():
+    pyautogui.PAUSE = 0.3
     pyautogui.hotkey('ctrl', 'c')
-    time.sleep(.01)
+    time.sleep(.02)
     text = str(pyperclip.paste())
     if "0.01" in text:
         for _ in range(5):
-            pyautogui.press('left')
+            pyautogui.PAUSE = 0.1
             for _ in range(4):
                 pyautogui.press('backspace')
+            pyautogui.press('left')
+            time.sleep(.02)
+        pyautogui.PAUSE = 0.4
         for _ in range(5):
             pyautogui.press('right')
+            time.sleep(.02)
     pyautogui.press('up')
+
+
+def name_check(last_row_with_names):
+    input("...\n...\nMove your mouse to the first student's name and press Enter")
+    excel_names = [str(score).strip() for score in xw.Range(f'B11:B{last_row_with_names}').value]
+    names = []
+    rejects = []
+    pyautogui.click(pyautogui.position())
+    while True:
+        pyautogui.hotkey('ctrl', 'c')
+        time.sleep(.02)
+        name_text = str(pyperclip.paste()).strip()
+        if len(names) > 0:
+            if name_text == names[-1]:
+                break
+        if name_text not in excel_names:
+            rejects.append(name_text)
+        names.append(name_text)
+        pyautogui.press('down')
+        return names, rejects
+
+# To Do!!!
+# - Finish and test name name_check()
+# - Fix the cleanup() function - it needs to move left
+# and then run up the rows
 
 
 if __name__ == '__main__':
@@ -72,6 +102,8 @@ if __name__ == '__main__':
     print("Make sure that you have your desired Excel file open, and the correct sheet selected,")
     input("then press 'Enter'")
     last_row = input("...\n...\nWhat is the last row occupied by students? ")
+    print("Checking that names line up")
+    names, rejects = name_check(last_row)
     while True:
         blank = input("...\n...\nIs the spreadsheet you will be copying the information into currently blank? (Y/N)"
                       "\n(If you are copying into a spreadsheet app that supports overwriting cells, just select Y) ")
@@ -109,5 +141,9 @@ if __name__ == '__main__':
                 copy_paste(grade_dict['scores'], reverse=True)
             else:
                 copy_paste(grade_dict['scores'], reverse=True, blank=False)
+    time.sleep(.02)
+    pyautogui.press('left')
     for _ in range(count):
         cleanup()
+
+
